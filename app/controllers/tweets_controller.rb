95092d23c6285ae060788837c66e9ba8ddfde37c
Feng Liu <me@liuf.net>
2013-06-05 02:54:10 -0500

    Remove show view and controller. Don't need them.

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -3,9 +3,4 @@ class TweetsController < ApplicationController
   def index
     @tweets = Tweet.order('id DESC').page(params[:page])
   end
-
-  # GET /tweets/1
-  def show
-    @tweet = Tweet.find(params[:id])
-  end
 end
